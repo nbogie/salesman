a7cda4bb59330ecff72a2d0c483f3eb1f9ddaa5d
Neill Bogie <neillbogie@googlemail.com>
2017-04-07 16:27:04 +0100

    allow individual words to be changed on a click

--- js/index.js
+++ js/index.js
@@ -7,23 +7,40 @@ var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/15U4k_wcpe-DK
 function init() {
   Tabletop.init( { key: publicSpreadsheetUrl,
                   callback: handleResponse
-                  } )
+                  } );
+
+  
+  [1,2,3,4,5,6].forEach(function(i) {  
+    var tag = $('#word' + i);
+    tag.click(function() {
+      replaceWithNewWord(gWords, tag);
+    });
+  });
+
+  $('#customer').click(function() {
+    replaceWithNewWord(gCustomers, $('#customer'));
+  });
+
+  
+  $('#randomise').click(randomise);
+
 }
+
 
 function handleResponse(data, tabletop) {
   
   gWords = tabletop.sheets('words').elements;
   gCustomers = tabletop.sheets('customers').elements;
   
-  $('#randomise').click(randomise);
   
   setNames = _.union(_.map(gWords, function(entry){ 
     return entry.pack; 
   }));
   
   setNames.forEach(function(si) {
-        $("<option/>").val(si).text(si).appendTo("#set-select");
-    });
+    $("<option/>").val(si).text(si).appendTo("#set-select");
+  });
+  randomise();
 }
 
 function classForPack(packName, defaultClass) {
@@ -32,6 +49,16 @@ function classForPack(packName, defaultClass) {
   return (ix >= 0) ? alloweds[ix] : defaultClass;
 }
 
+function placeWordIn(w, tag) {
+  tag.html(w.title + " ");
+  tag.removeClass();
+  tag.addClass(classForPack(w.pack, 'noPack'));
+}
+
+function replaceWithNewWord(list, tag){
+  var w = _.sample(list);
+  placeWordIn(w, tag);
+}
 
 function randomise() {
   setName = $('#set-select').val();  
@@ -48,15 +75,10 @@ function randomise() {
   
   cust = _.sample(gCustomers);
   words = _.sample(gWords, 5).concat(_.sample(interestingWords, 1));
-  var i = 0;
-  for (i = 0; i < 6; i++) {
-    var w = words[i];
-    cls = classForPack(w.pack, 'noPack');
-    tag = $('#word'+(i+1));
-    tag.html(w.title + " ");
-    tag.removeClass();
-    tag.addClass(cls);    
+  for (var i = 0; i < 6; i++) {
+    placeWordIn(words[i], $('#word'+(i+1)));
   }
+
   $('#customer').html(cust.title).
     removeClass().
     addClass(classForPack(cust.pack, 'noPack'));
